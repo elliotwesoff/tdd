ecd52d72fd25f647f51cb24f3bd507e8f33b8035
Elliot Wesoff <ewesoff@gmail.com>
2024-02-10 18:45:29 -0800

    fix all linter errors.

--- src/counter.py
+++ src/counter.py
@@ -8,15 +8,18 @@ COUNTERS = {}
 # specify the variable in route <name>
 # let Flask know that the only methods that is allowed to called
 # on this function is "POST".
+
+
 @app.route('/counters/<name>', methods=['POST'])
 def create_counter(name):
     """Create a counter"""
     app.logger.info(f"Request to create counter: {name}")
     global COUNTERS
     if name in COUNTERS:
-        return {"Message":f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
+        return {"Message": f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
     COUNTERS[name] = 0
     return {name: COUNTERS[name]}, status.HTTP_201_CREATED
+
 
 @app.route('/counters/<name>', methods=['PUT'])
 def update_counter(name):
@@ -26,7 +29,8 @@ def update_counter(name):
         COUNTERS[name] += 1
         return {name: COUNTERS[name]}, status.HTTP_200_OK
     else:
-        return {"Message":f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
+        return {"Message": f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
+
 
 @app.route('/counters/<name>', methods=['GET'])
 def get_counter(name):
@@ -35,5 +39,4 @@ def get_counter(name):
     if name in COUNTERS:
         return {name: COUNTERS[name]}, status.HTTP_200_OK
     else:
-        return {"Message":f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
-
+        return {"Message": f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
